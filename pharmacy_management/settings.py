344e6fd96a0d780e6198bb3870877d154d7fc9db
Suraj Jha <jhasuraj10000@gmail.com>
2026-07-25 12:39:20 +0545

    setting

--- pharmacy_management/settings.py
+++ pharmacy_management/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -92,7 +94,7 @@ DATABASES = {
 
         'HOST': 'localhost',
 
-        'PORT': '3306',
+        'PORT': '3307',
 
     }
 }
@@ -181,3 +183,9 @@ SIMPLE_JWT = {
     True,
 
 }
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+]
