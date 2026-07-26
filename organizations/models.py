e8bb426db941eec1f714c88c095e110363b6c34c
Suraj Jha <jhasuraj10000@gmail.com>
2026-07-26 14:41:15 +0545

    added organization profile

--- organizations/models.py
+++ organizations/models.py
@@ -3,13 +3,26 @@ import uuid
 
 # Create your models here.
 class Organization(models.Model):
+
+    #Basic Infromation
     name=models.CharField(max_length=200)
     organization_id = models.CharField(max_length=20,unique=True,blank=True,null=True)
+    established_date=models.DateField(blank=True,null=True)
+
+    #Contact Infromation
+
     address=models.TextField(blank=True,null=True)
     phone=models.CharField(max_length=20,blank=True,null=True)
     email=models.EmailField(blank=True,null=True)
+
+    #pharmacy Legal information
+    registration_number= models.CharField(max_length=100,blank=True,null=True)
+    license_number= models.CharField(max_length=100,blank=True,null=True)
+    pan_number= models.CharField(max_length=100,blank=True,null=True)
+    
     is_active=models.BooleanField(default=True)
     created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
     def save(self,*args,**kwargs):
 
